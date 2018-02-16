aab8b760e2ea6f02c03d4ea5bc27a7e0b7d05e98
DrN <DrN@DESKTOP-GORGVHP>
2018-02-15 20:14:18 -0800

    Finished product

--- COMP_3512_Lab5/COMP_3512_Lab5/RPMTester.cpp
+++ COMP_3512_Lab5/COMP_3512_Lab5/RPMTester.cpp
@@ -8,9 +8,20 @@ using namespace std;
 
 int main() {
 	RPNCalculator * test = new RPNCalculator();
+	//example cases
+	cout << "Example Test Cases " << endl;
+	cout << "2 3 4 * -" << endl;
 	cout << test->process_form("2 3 4 * -") << endl;
+	cout << "2 3 - 4 *" << endl;
 	cout << test->process_form("2 3 - 4 *") << endl;
 	
+	cout << "Enter your formula: " << endl;
+	string formula;
+	getline(std::cin, formula);
+	cout << "You entered " << formula << endl;
+	cout << "The result is " << endl;
+	cout << test->process_form(formula) << endl;
+
 	system("pause");
 	return 0;
 }
